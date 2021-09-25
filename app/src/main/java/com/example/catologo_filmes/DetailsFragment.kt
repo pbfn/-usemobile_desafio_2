c948387d3efe58aed30ea0028fe13f8aaf03cd5b
Pedro <pbruno1283@gmail.com>
2021-09-24 22:57:52 -0300

    Binding adicionado aos fragments

--- app/src/main/java/com/example/catologo_filmes/DetailsFragment.kt
+++ app/src/main/java/com/example/catologo_filmes/DetailsFragment.kt
@@ -5,9 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.catologo_filmes.databinding.FragmentDetailsBinding
 
 class DetailsFragment : Fragment() {
 
+    private  var _binding: FragmentDetailsBinding?= null
+    private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,7 +20,8 @@ class DetailsFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_details, container, false)
+        _binding = FragmentDetailsBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
 }
--- app/src/main/java/com/example/catologo_filmes/MainActivity.kt
+++ app/src/main/java/com/example/catologo_filmes/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.catologo_filmes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.example.catologo_filmes.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        binding.buttonVoltar.visibility = View.GONE
     }
 }
