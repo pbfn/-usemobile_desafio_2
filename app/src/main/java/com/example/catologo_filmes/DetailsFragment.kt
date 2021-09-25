38f2cad7b962ceb6771668357fc1eaa0786bf025
Pedro <pbruno1283@gmail.com>
2021-09-24 23:06:08 -0300

    Titulo da toolbar adicionado em cada fragment

--- app/src/main/java/com/example/catologo_filmes/DetailsFragment.kt
+++ app/src/main/java/com/example/catologo_filmes/DetailsFragment.kt
@@ -5,12 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import com.example.catologo_filmes.databinding.FragmentDetailsBinding
 
 class DetailsFragment : Fragment() {
 
     private  var _binding: FragmentDetailsBinding?= null
     private val binding get() = _binding!!
+    private var buttonVoltar: ImageView? = null
+    private var textViewToolbar: TextView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,6 +26,18 @@ class DetailsFragment : Fragment() {
     ): View? {
         _binding = FragmentDetailsBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        setToolbar()
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    private fun setToolbar(){
+        buttonVoltar = activity?.findViewById(R.id.button_voltar)
+        buttonVoltar?.visibility = View.GONE
+        textViewToolbar = activity?.findViewById(R.id.text_view_toolbar)
+        textViewToolbar?.text =String.format(R.string.details_title.toString())
     }
 
 }
--- app/src/main/java/com/example/catologo_filmes/HomeFragment.kt
+++ app/src/main/java/com/example/catologo_filmes/HomeFragment.kt
@@ -5,13 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import com.example.catologo_filmes.databinding.FragmentHomeBinding
 
 
 class HomeFragment : Fragment() {
 
-    private  var _binding: FragmentHomeBinding?= null
+    private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
+    private var buttonVoltar: ImageView? = null
+    private var textViewToolbar: TextView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,6 +27,18 @@ class HomeFragment : Fragment() {
     ): View? {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        setToolbar()
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    private fun setToolbar() {
+        buttonVoltar = activity?.findViewById(R.id.button_voltar)
+        buttonVoltar?.visibility = View.GONE
+        textViewToolbar = activity?.findViewById(R.id.text_view_toolbar)
+        textViewToolbar?.text = String.format(R.string.app_name.toString())
     }
 
 }
